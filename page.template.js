a305abfcbe63422bcb9fa5c43129aee939dd8c86
Adrien Pinet <apinet@users.noreply.github.com>
2023-11-26 17:57:10 +0100

    update sdk

--- page.template.js
+++ page.template.js
@@ -24,6 +24,7 @@ const createFaviconTags = () => {
   return `
     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png"/>
     <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
+    <link rel="icon" type="image/svg+xml" href="/static/favicon.ico"/>
     <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/static/apple-touch-icon.png"/>
     <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#7928ca"/>
   `;
