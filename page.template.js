aeed36002927ef0c608fc649be5267d316ee21de
Adrien Pinet <apinet@users.noreply.github.com>
2024-01-18 18:57:24 +0100

    add theme-color meta tag

--- page.template.js
+++ page.template.js
@@ -34,7 +34,10 @@ export const index = (pageDesc, sdkVersion, appVersion) => `
 <html lang="en">
   <head>
     <meta charset="utf-8" />
+    
     <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, viewport-fit=cover" />
+    <meta name="theme-color" content="#3b82f6" />
+    
     <meta name="sdk-version" content="${sdkVersion}" />
     <meta name="app-version" content="${appVersion}" />
 
